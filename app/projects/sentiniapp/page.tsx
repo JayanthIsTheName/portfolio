ed063e22042ea02ef389a62a17eb48fabef429f0
JayanthIsTheName <jayanthisthename@gmail.com>
2024-02-25 11:53:14 +0530

    sentini-app

---
+++ app/projects/sentiniapp/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import styles from "./styles.module.css"
+import React from "react";
+import { WavyBackground } from "../../components/ui/wavy-background";;
+import { ParallaxScroll } from "../../components/ui/parallax-scroll";
+
+export default function sentiniapp() {
+    const images = [
+        "/catalog.png",
+        "/createsale.png",
+        "/dashboard.png",
+        "/home.png",
+        "/ledger.png",
+        "/profile.png",
+        "/purchaseregister.png",
+        "/getmoney.png"
+      ];
+    let h = document.documentElement.clientHeight/1.5
+    return (
+        <div>
+            <div className={styles.sentini_app} >
+                <WavyBackground  className="mx-auto pb-40 ">
+                    <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+                        Sentini Mobile App
+                    </p>
+                    <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+                        This is the sentini mobile app
+                    </p>
+                </WavyBackground>
+                
+            </div>
+            <p className="text-center text-[30px]">Slides</p>
+            <ParallaxScroll images={images} />
+            <div className="text-red-400">
+                next project
+            </div>
+        </div>
+    )
+}
